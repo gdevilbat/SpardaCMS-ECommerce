4077567a70ca8e6be07eaea7b92f9c6374ced156
unknown <maldinuribrahim@gmail.com>
2019-12-26 15:39:46 +0700

    Post Repository Service

--- Http/Controllers/ProductController.php
+++ Http/Controllers/ProductController.php
@@ -22,11 +22,10 @@ class ProductController extends AbstractPost
      * Display a listing of the resource.
      * @return Response
      */
-    public function __construct()
+    public function __construct(\Gdevilbat\SpardaCMS\Modules\Post\Repositories\PostRepository $post_repository)
     {
-        parent::__construct();
+        parent::__construct($post_repository);
         $this->post_m = new Product;
-        $this->post_repository = new Repository(new Product);
         $this->product_meta_m = new ProductMeta;
         $this->product_meta_repository = new Repository(new ProductMeta);
 
