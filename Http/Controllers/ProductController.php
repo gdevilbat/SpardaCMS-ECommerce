d736062b05c6c8f0ea1a358640a97c599d60a5b9
maldinuribrahim <maldinuribrahim@gmail.com>
2020-12-29 01:22:45 +0700

    Fix Column Order

--- Http/Controllers/ProductController.php
+++ Http/Controllers/ProductController.php
@@ -35,7 +35,7 @@ class ProductController extends AbstractPost
 
     public function getColumnOrder()
     {
-        return ['', '', Product::getPrimaryKey(), 'post_title', 'author', 'categories', 'tags','comment', 'post_status', 'product_price', 'product_sale', 'tokopedia_slug', 'tokopedia_source', 'created_at'];
+        return ['', '', Product::getPrimaryKey(), 'post_title', 'author', 'categories', 'tags','comment', 'post_status', 'product_price', 'product_sale', 'tokopedia_slug', 'tokopedia_source', 'availability', 'created_at'];
     }
 
     public function getQuerybuilder($column, $dir)
