534e2a83775e6aa970a432cb01aed1a22104b58f
maldinuribrahim <maldinuribrahim@gmail.com>
2021-07-14 07:46:41 +0700

    Hide Parameter Scrapping

--- preprocessor/js/scrapper.js
+++ preprocessor/js/scrapper.js
@@ -187,11 +187,12 @@ window.tokopediaScrap = function(){
                         {
                             config = {
                               method: 'post',
-                              url: $("#data_product").attr('data-url-scrapping-shopee')+'?shopid='+shopee[0]+'&itemid='+shopee[1],
+                              url: $("#data_product").attr('data-url-scrapping-shopee'),
                               headers: {
                                 "Accept": "application/json",
                                 "Authorization": "Bearer "+ $("meta[name='api-token']").attr('content') 
-                              }
+                              },
+                              data: {'shopid': shopee[0], 'itemid': shopee[1]}
                             };
                         }
                         else
@@ -240,14 +241,6 @@ window.tokopediaScrap = function(){
 
                                     }
                             }, (error) => console.log(err) );
-
-                        /*let config = {
-                          method: 'get',
-                          url: 'https://seller.shopee.co.id/api/v3/product/get_product_detail/?product_id='+shopee[1],
-                          headers: { 
-                            'cookie': 'fbm_957549474255294=base_domain=.shopee.co.id; cto_lwid=195b1764-0ef0-4bec-aa92-ac4a603bf9df; __utma=156485241.1231309772.1569496850.1571747440.1571747440.1; SPC_F=fEh1EGf8MiXYqPtpJU3Y4wQTKYud6itt; _gcl_au=1.1.2058148069.1591685537; _fbp=fb.2.1591685537834.1900528078; SC_DFP=L3uPlm1FDKc30axfxqaCuaC3rxO79u1I; G_ENABLED_IDPS=google; SPC_CDS=51f3ab59-1df7-4ae0-b9d8-2bce7d03ca4c; UYOMAPJWEMDGJ=; SPC_SC_SA_TK=; SPC_SC_SA_UD=; SPC_SC_TK=92229a9f904347e62e3685971bfb8964; SPC_WST="d+WJMbfdAZxq8o6f1ZZdUFSKsQVVfK+WLPrrKJbwUGW7HJP6BvMzLMvreoPN/5T71XzIKkv+cSdwm7rMWPOLv/QT5xuzNf7hj3gdzNg7CuW/YskfPO+S3KvHDyUMc3IdWQ1bCEgzpz3wI0cgqizcFsznxnC1HmZk4rnQZSZo+Vc="; SPC_SC_UD=89948237; SPC_U=89948237; SPC_EC=d+WJMbfdAZxq8o6f1ZZdUFSKsQVVfK+WLPrrKJbwUGW7HJP6BvMzLMvreoPN/5T71XzIKkv+cSdwm7rMWPOLv/QT5xuzNf7hj3gdzNg7CuW/YskfPO+S3KvHDyUMc3IdWQ1bCEgzpz3wI0cgqizcFsznxnC1HmZk4rnQZSZo+Vc=; AMP_TOKEN=%24NOT_FOUND; _gid=GA1.3.596009003.1598518667; SPC_SI=3abmbd01glvqb7yk94y6knoj6wlia9tq; _med=refer; _ga=GA1.1.1231309772.1569496850; _ga_SW6D8G0HXK=GS1.1.1598518666.5.1.1598520168.0; SPC_R_T_ID="qGo1kOmHqovN6BRYoVfDJfBnOcyr010+Xd4t6vVF41H7aZ49kfg4Zl4YWjKFO9pau2YxuUCORqb4EqaoT1S+sOiWAMpRqw9c6sxGy5LluII="; SPC_R_T_IV="HMPB0wAL55/voizsjYIyqQ=="'
-                          }
-                        };*/
 
                         let config_detail = {
                           method: 'post',
